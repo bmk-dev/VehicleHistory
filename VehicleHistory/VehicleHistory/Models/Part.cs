00bf872b0baac6cb186ddfe969986194a0291608
bmk <straysify@gmail.com>
2022-02-17 20:41:03 -0500

    Create Part

---
+++ VehicleHistory/VehicleHistory/Models/Part.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleHistory.Models
+{
+    [Serializable]
+    public class Part
+    {
+        public string PartNumber { get; set; }
+        public int Cost { get; set; }
+        public int Quantity { get; set; }
+    }
+}
