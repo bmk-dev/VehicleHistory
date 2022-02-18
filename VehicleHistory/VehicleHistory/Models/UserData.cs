550236e302e95560950b77997ef49cb712847489
bmk <straysify@gmail.com>
2022-02-17 20:41:14 -0500

    Create UserData

---
+++ VehicleHistory/VehicleHistory/Models/UserData.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleHistory.Models
+{
+    [Serializable]
+    internal class UserData
+    {
+        //public List<Vehicle> UserVehicles { get; set; }
+
+        public List<FuelRecord> FuelRecords { get; set; }
+        public List<Service> ServiceHistory { get; set; } 
+
+        [JsonConstructor]
+        public UserData() { }
+    }
+}
