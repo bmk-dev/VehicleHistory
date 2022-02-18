5a2540b9a7c9254defa785d723cfa53b6076013d
bmk <straysify@gmail.com>
2022-02-17 20:40:56 -0500

    Create FuelRecord

--- VehicleHistory/VehicleHistory/Models/FuelRecord.cs
+++ VehicleHistory/VehicleHistory/Models/FuelRecord.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace VehicleHistory.Models
 {
+    [Serializable]
     public class FuelRecord
     {
-        public DateTime DateTime { get; set; }
         public string Id { get; set; }
+
+        // The mileage when pumped
         public int Mileage { get; set; }
+
+        // Number of gallons pumped
         public float GallonsPumped { get; set; }
+
+
+        [JsonConstructor]
+        public FuelRecord()
+        {
+
+        }
 
     }
 }
