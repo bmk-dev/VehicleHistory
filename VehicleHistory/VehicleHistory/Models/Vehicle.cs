59e01e860f15de0f3dbf8d9a0deb0a3e71fdd296
bmk <straysify@gmail.com>
2022-02-17 20:41:20 -0500

    Create Vehicle

---
+++ VehicleHistory/VehicleHistory/Models/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleHistory.Models
+{
+    [Serializable]
+    internal class Vehicle
+    {
+        public string Id { get; set; }
+        public HashSet<FuelRecord> FuelRecords { get; set; }
+        public HashSet<Service> ServiceHistory { get; set; }
+
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+
+
+        [JsonConstructor]
+        public Vehicle() { }
+
+
+    }
+}
