94296b45a68ae86c21e66f33df7fd6df239734af
bmk <straysify@gmail.com>
2022-02-17 20:41:09 -0500

    Create Service

--- VehicleHistory/VehicleHistory/Models/Service.cs
+++ VehicleHistory/VehicleHistory/Models/Service.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace VehicleHistory.Models
 {
+    [Serializable]
     public class Service
     {
         public string Id { get; set; }
         public string Title { get; set; }
         public int Mileage { get; set; }
         public string Description { get; set; }
+
+        public List<Part> Parts { get; set; }
+
+
+
+
+        [JsonConstructor]
+        public Service()
+        {
+
+        }
     }
 }
